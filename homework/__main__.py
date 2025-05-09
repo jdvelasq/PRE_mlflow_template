0bad7434e4433f42e6c5cc6d9f43bdeb57f7a050
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-05-08 19:27:41 -0500

    Initial commit

---
+++ homework/__main__.py
@@ -0,0 +1,10 @@
+"""Entry point for the homework package."""
+
+# python3 -m homework data/input data/output
+
+from .src.__solucion__ import main
+
+# from .src.main import main
+
+if __name__ == "__main__":
+    main()
